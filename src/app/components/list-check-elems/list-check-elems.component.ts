7cf50ffb8c38bb48cfe278266c5309974cd53cdc
Polina Deniskina <polinaoioi67@gmail.com>
2024-12-17 11:29:32 +0300

    feat: add functions for button

--- src/app/components/list-check-elems/list-check-elems.component.ts
+++ src/app/components/list-check-elems/list-check-elems.component.ts
@@ -1,9 +1,12 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {DataService} from '../../services/data.service';
+import { ChangeDetectionStrategy, Component, computed, ElementRef, HostListener } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-list-check-elems',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './list-check-elems.component.html',
   styleUrl: './list-check-elems.component.css',
   standalone: true,
@@ -11,10 +14,42 @@ import {DataService} from '../../services/data.service';
 })
 export class ListCheckElemsComponent {
 
-  constructor(protected data: DataService) {}
+  buttonText$ = computed(() =>
+    `${this.data.getPluralForm(this.data.funnelsCount(), [
+      'воронка',
+      'воронки',
+      'воронок',
+    ])}, ${this.data.getPluralForm(this.data.stagesCount(), [
+      'этап',
+      'этапа',
+      'этапов',
+    ])}`
+  );
+
+  constructor(protected data: DataService, private elementRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+    if (!clickedInside && this.data.isMenuOpen()) {
+      this.data.isMenuOpen.set(false);
+    }
+  }
 
   onCheckBoxChange(list: string, key: string, event: Event) {
     const checkbox = event.target as HTMLInputElement;
     this.data.updateCheckboxState(list, key, checkbox.checked);
+  }
+
+  onButtonClick() {
+    if (!this.data.isMenuOpen()) {
+      this.data.toggleMenu();
+    } else {
+      if (this.data.isAllUnchecked()) {
+        this.data.selectAll();
+      } else {
+        this.data.deselectAll();
+      }
+    }
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import {computed, Injectable, signal} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -6,43 +6,78 @@ import { Injectable, signal } from '@angular/core';
 export class DataService {
 
   salesState = signal([
-    { key: 'notProcessed', label: 'Неразобрано', checked: false },
-    { key: 'negotiations', label: 'Переговоры', checked: false },
-    { key: 'decisionPending', label: 'Принимают решение', checked: false },
-    { key: 'successful', label: 'Успешно', checked: false },
+    {key: 'notProcessed', label: 'Неразобрано', checked: false},
+    {key: 'negotiations', label: 'Переговоры', checked: false},
+    {key: 'decisionPending', label: 'Принимают решение', checked: false},
+    {key: 'successful', label: 'Успешно', checked: false},
   ]);
 
   employeesState = signal([
-    { key: 'notProcessed', label: 'Неразобрано', checked: false },
-    { key: 'negotiations', label: 'Переговоры', checked: false },
-    { key: 'decisionPending', label: 'Принимают решение', checked: false },
-    { key: 'successful', label: 'Успешно', checked: false },
+    {key: 'notProcessed', label: 'Неразобрано', checked: false},
+    {key: 'negotiations', label: 'Переговоры', checked: false},
+    {key: 'decisionPending', label: 'Принимают решение', checked: false},
+    {key: 'successful', label: 'Успешно', checked: false},
   ]);
 
   partnersState = signal([
-    { key: 'notProcessed', label: 'Неразобрано', checked: false },
-    { key: 'negotiations', label: 'Переговоры', checked: false },
-    { key: 'decisionPending', label: 'Принимают решение', checked: false },
-    { key: 'successful', label: 'Успешно', checked: false },
+    {key: 'notProcessed', label: 'Неразобрано', checked: false},
+    {key: 'negotiations', label: 'Переговоры', checked: false},
+    {key: 'decisionPending', label: 'Принимают решение', checked: false},
+    {key: 'successful', label: 'Успешно', checked: false},
   ]);
 
   eventState = signal([
-    { key: 'notProcessed', label: 'Неразобрано', checked: false },
-    { key: 'negotiations', label: 'Переговоры', checked: true },
-    { key: 'decisionPending', label: 'Принимают решение', checked: false },
-    { key: 'successful', label: 'Успешно', checked: false },
+    {key: 'notProcessed', label: 'Неразобрано', checked: false},
+    {key: 'negotiations', label: 'Переговоры', checked: true},
+    {key: 'decisionPending', label: 'Принимают решение', checked: false},
+    {key: 'successful', label: 'Успешно', checked: false},
   ]);
 
   incomingRequestsState = signal([
-    { key: 'notProcessed', label: 'Неразобрано', checked: false },
-    { key: 'negotiations', label: 'Переговоры', checked: false },
-    { key: 'decisionPending', label: 'Принимают решение', checked: false },
-    { key: 'successful', label: 'Успешно', checked: false },
+    {key: 'notProcessed', label: 'Неразобрано', checked: false},
+    {key: 'negotiations', label: 'Переговоры', checked: false},
+    {key: 'decisionPending', label: 'Принимают решение', checked: false},
+    {key: 'successful', label: 'Успешно', checked: false},
   ]);
+
+  isMenuOpen = signal(false);
+
+  private allStates = computed(() => [
+    this.salesState(),
+    this.employeesState(),
+    this.partnersState(),
+    this.eventState(),
+    this.incomingRequestsState()
+  ]);
+
+  funnelsCount = computed(() =>
+    this.allStates().filter((state) =>
+      state.some((checkbox) => checkbox.checked)
+    ).length
+  );
+
+  stagesCount = computed(() =>
+    this.allStates().reduce((acc, state) =>
+      acc + state.filter((checkbox) => checkbox.checked).length, 0)
+  );
+
+  /**
+   * Универсальная функция для склонения слов
+   * @param count number
+   * @param words string[]
+   */
+  getPluralForm(count: number, words: [string, string, string]): string {
+    const cases = [2, 0, 1, 1, 1, 2];
+    const index =
+      count % 100 > 4 && count % 100 < 20
+        ? 2
+        : cases[Math.min(count % 10, 5)];
+    return `${count} ${words[index]}`;
+  }
 
   updateCheckboxState(list: string, key: string, value: boolean) {
     // @ts-ignore
-    const state  = this[list];
+    const state = this[list];
 
     if (state) {
       const currentState = state();
@@ -53,5 +88,37 @@ export class DataService {
       }
     }
     console.log(state());
+  }
+
+  isAllUnchecked() {
+    return this.allStates().every((state) =>
+      state.every((checkbox) => !checkbox.checked)
+    );
+  }
+
+  toggleMenu() {
+    this.isMenuOpen.set(!this.isMenuOpen());
+  }
+
+  selectAll() {
+    this.allStates().forEach((state) => {
+      state.forEach((checkbox) => (checkbox.checked = true));
+    });
+    this.updateSignals();
+  }
+
+  deselectAll() {
+    this.allStates().forEach((state) => {
+      state.forEach((checkbox) => (checkbox.checked = false));
+    });
+    this.updateSignals();
+  }
+
+  private updateSignals() {
+    this.salesState.set([...this.salesState()]);
+    this.employeesState.set([...this.employeesState()]);
+    this.partnersState.set([...this.partnersState()]);
+    this.eventState.set([...this.eventState()]);
+    this.incomingRequestsState.set([...this.incomingRequestsState()]);
   }
 }
